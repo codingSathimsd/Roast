b2f738007f0c4c64773b0b40611d81506ccd9d1b
codingSathimsd <codingsathimsd@gmail.com>
2026-04-15 07:31:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,6 @@ window.API_URL = "https://roastmyidea.deno.dev";
 // 2. Paste your Google Client ID here
 // Get from: console.cloud.google.com
 // → APIs & Services → Credentials → OAuth 2.0 Client ID
-window.GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID.apps.googleusercontent.com";
+window.GOOGLE_CLIENT_ID = "1064427626441-
+
+b2p90mv48g7mfnte5qupf900f3t8 cs4u.apps.googleusercontent.com.apps.googleusercontent.com";
